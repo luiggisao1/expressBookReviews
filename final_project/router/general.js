e6c8ccc02c71f6633da83fd963e196934d46789f
Luiggi Alarcon <luiggis.ao@hotmail.com>
2025-02-28 22:05:15 -0500

    Use axios in task 1-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,8 +1,10 @@
+const axios = require("axios");
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const url = "https://raw.githubusercontent.com/luiggisao1/expressBookReviews/refs/heads/main/final_project/router/booksdb.json"
 
 
 public_users.post("/register", (req,res) => {
@@ -20,13 +22,19 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.status(200).json(books);
+public_users.get('/', async function (req, res) {
+  const response = await axios.get(url);
+  const books = response.data;
+
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
+  const response = await axios.get(url);
+  const books = response.data;
   const book = books[req.params.isbn];
+
   if (book) {
     return res.status(200).json(book);
   } else {
@@ -35,14 +43,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
+  const response = await axios.get(url);
+  const books = response.data;
   const filteredBook = Object.values(books).filter(book => book.author === req.params.author);
+
   return res.status(200).json(filteredBook);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
+  const response = await axios.get(url);
+  const books = response.data;
   const filteredBook = Object.values(books).find(book => book.title === req.params.title);
+
   if (filteredBook) {
     return res.status(200).json(filteredBook);
   } else {
